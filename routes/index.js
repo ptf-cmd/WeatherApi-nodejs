5f714d36648d1c47f311e49d87de8a59b5b5f41a
JuanIgnacioCuiule <juanignaciocuiule@gmail.com>
2016-10-04 08:39:27 -0300

    ip y geolocalización en pagina de inicio

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var geoip = require('geoip-lite');
+var request = require('request');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	var city;
+	request('https://api.ipify.org?format=json', (err, response, body) => {
+		if (!err && response.statusCode == 200) {
+			console.log(body);
+			body = JSON.parse(body);
+			var ip = body.ip;
+			console.log(ip);
+			var geo = geoip.lookup(ip);
+			console.log(geo);
+			city = geo.city;
+			res.render('index', { title: city });
+		};
+	});
 });
 
 module.exports = router;
